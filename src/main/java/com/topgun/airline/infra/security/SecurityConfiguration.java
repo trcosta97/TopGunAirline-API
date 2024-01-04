6c8c42c79dbcb61c2694cbc1e43a72b23963927a
Thiago Ribeiro <trcosta97@hotmail.com>
2024-01-04 13:59:07 -0300

    oermit all get

--- src/main/java/com/topgun/airline/infra/security/SecurityConfiguration.java
+++ src/main/java/com/topgun/airline/infra/security/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration {
                 .cors()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/chat").permitAll()
+                .requestMatchers(HttpMethod.GET, "/flight").permitAll()
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
